652e7899f764f6cd9ce03753fc203fd37c9364f2
GONZALEZ GALLEGOS Edwin <g15023670@A-F001925-L.salsa.univ-amu.fr>
2016-04-04 17:17:39 +0200

    Entrust

--- app/Role.php
+++ app/Role.php
@@ -3,8 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Zizaco\Entrust\EntrustRole;
+ 
+class Role extends EntrustRole {
 
-class Role extends Model
-{
-    //
 }
--- app/User.php
+++ app/User.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+use Zizaco\Entrust\HasRole;
 
-class User extends Authenticatable
+class User extends Eloquent implements UserInterface, RemindableInterface 
 {
+
+use HasRole;
     /**
      * The attributes that are mass assignable.
      *
@@ -23,4 +30,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    $create_user = new Permission();
+    $create_user->name = 'create_user';
+    $create_user->display_name = 'crear usuarios';
+    $create_user->save();
+ 
+    $admin = new Role();
+    $admin->name = 'admin';
+    $admin->save();
+    $admin->attachPermission($create_user);
+    $user = User::find(1);
+    $user->attachRole($admin);
 }
