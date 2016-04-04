89b0a633dc3bd71549caabaa1abdb730de24562d
GONZALEZ GALLEGOS Edwin <g15023670@A-F001925-L.salsa.univ-amu.fr>
2016-04-04 16:18:35 +0200

    Sentinel

--- config/app.php
+++ config/app.php
@@ -156,7 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        'Cartalyst\Sentinel\Laravel\SentinelServiceProvider',
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
 
     ],
 
@@ -204,10 +204,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Activation' => 'Cartalyst\Sentinel\Laravel\Facades\Activation',
-        'Reminder'   => 'Cartalyst\Sentinel\Laravel\Facades\Reminder',
-        'Sentinel'   => 'Cartalyst\Sentinel\Laravel\Facades\Sentinel',
-
+        'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
     ],
 
 ];
