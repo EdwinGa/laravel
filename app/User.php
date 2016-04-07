590610a439fcb4141fa2b7fdf7f45dce556602cf
GONZALEZ GALLEGOS Edwin <g15023670@A-F001925-L.salsa.univ-amu.fr>
2016-04-07 09:18:38 +0200

    Entrust

--- app/User.php
+++ app/User.php
@@ -9,7 +9,7 @@ use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Zizaco\Entrust\HasRole;
 
-class User extends Eloquent implements UserInterface, RemindableInterface 
+class User extends Authenticatable implements UserInterface, RemindableInterface 
 {
 
 use HasRole;
